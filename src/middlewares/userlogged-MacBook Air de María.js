b3ce13f90f238423d99a0512917491f5df4ac854
mariadumas <dumasmaria@gmail.com>
2022-06-22 19:39:06 -0300

    archivos dobles eliminados

--- src/middlewares/userlogged-MacBook Air de María.js
+++
@@ -1,8 +0,0 @@
-function userLogged(req, res, next) {
-	if (req.session.usuarioLogeado) {   //si en el req en session tengo a alguien redirigir al profile
-		return res.redirect('/user/profile'); //si es verdadero redirect 
-	}
-	next(); //si es falso next
-}
-
-module.exports = userLogged;
