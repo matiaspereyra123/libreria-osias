acbefd7b13d55f737bd19eeef5b3482ae8fe97e2
mariadumas <dumasmaria@gmail.com>
2022-07-13 00:17:03 -0300

    vista viajes

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -6,5 +6,6 @@ const router = express.Router()
 const mainController = require("../controllers/mainController")
 
 router.get("/", mainController.home);
+router.get('/viajes', mainController.viajes);
 
 module.exports = router
