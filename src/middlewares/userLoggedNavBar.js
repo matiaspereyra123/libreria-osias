044488e559a87a52affa7895b5f1c576513c1cd0
matiaspereyra123 <raulmatiaspereyra@gmail.com>
2022-07-16 17:30:19 -0300

    Funcionalidades Admin sin tildar  Recordar

--- src/middlewares/userLoggedNavBar.js
+++ src/middlewares/userLoggedNavBar.js
@@ -3,7 +3,7 @@ const db = require("../database/models");
 function userLoggedBlockNav(req,res,next){ 
     res.locals.userLogged =  false;
     let emailEnCookie = req.cookies.recordar;
-    if(req.cookies.recordar || req.session){
+    if(req.cookies.recordar || req.session.usuarioLogeado){
         if(emailEnCookie){
             let usuarioRegistrado =  db.Usuario.findOne({ where: { email: emailEnCookie} })
                 .then(usuario=>{
