843810f14eab00b08219cb17c8f89437a14003c4
mariadumas <dumasmaria@gmail.com>
2022-07-26 16:00:09 -0300

    header

--- public/js/userValidation/userValidation.js
+++ public/js/userValidation/userValidation.js
@@ -1,20 +1,20 @@
-window.onload = function(){
-    let formulario = document.querySelector('#formulario');
-    let formu= document.querySelector(".formulario-completo");
-    let email=document.getElementById("email");
-    let password=document.getElementById("password");
-    let errores=document.querySelector(".errores");
-    let emailVacio = document.getElementById("emailVacio");
+// window.onload = function(){
+//     let formulario = document.querySelector('#formulario');
+//     let formu= document.querySelector(".formulario-completo");
+//     let email=document.getElementById("email");
+//     let password=document.getElementById("password");
+//     let errores=document.querySelector(".errores");
+//     let emailVacio = document.getElementById("emailVacio");
 
-    formu.addEventListener("submit",function(e){
+//     formu.addEventListener("submit",function(e){
 
-    })
-    email.addEventListener("blur",function(){
-       if(email.value=''){
+//     })
+//     email.addEventListener("blur",function(){
+//        if(email.value=''){
   
-        errores.classList.add("is-invalid");
-       }
-    })
+//         errores.classList.add("is-invalid");
+//        }
+//     })
 
-}
+// }
 //probando
