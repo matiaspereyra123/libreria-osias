5a6e186849cff87926555448c9345a8b1e552317
matiaspereyra123 <raulmatiaspereyra@gmail.com>
2022-07-26 23:46:30 -0300

    validacion front login con styles terminado

--- public/js/userValidation/userValidation.js
+++ public/js/userValidation/userValidation.js
@@ -5,6 +5,8 @@ window.onload = function(){
     let parrafoEmail = document.getElementById('emailP');
     let parrafoPassword = document.getElementById('passwordP');
 
+    let iconEmail = document.getElementById('iconEmail');
+    let iconPassword=document.getElementById('iconPassword');
     let btnEnviar=document.getElementById('btnEnviar');
 
     btnEnviar.addEventListener("click",(e)=>{
@@ -29,13 +31,23 @@ window.onload = function(){
 
          if(campoEmail.value.match(expReg)){
              campoEmail.style.border="2.1px solid green";
-             parrafoEmail.style.display = "none"
+             parrafoEmail.style.display = "none";
+             
+                iconEmail.classList.remove('fa-circle-xmark');
+                iconEmail.classList.add('fa-circle-check');
+              iconEmail.style.visibility="visible";
+              iconEmail.style.color="green";
            // parrafoEmail.innerHTML=" FORMATO válido";
         }else{
             parrafoEmail.innerHTML="El correo no es un FORMATO válido";
             parrafoEmail.style.display="block";
             parrafoEmail.classList.add('is-invalid');
             campoEmail.style.border="2.1px solid Red";
+            iconEmail.classList.remove('fa-cicle-check');
+            iconEmail.classList.add('fa-circle-xmark');
+            iconEmail.style.visibility="visible";
+            iconEmail.style.color="red";
+        
         } 
    
     }
@@ -47,11 +59,20 @@ window.onload = function(){
             parrafoPassword.style.display="block";
             campoPassword.style.border="2.1px solid red";
             parrafoPassword.classList.add('is-invalid');
-            parrafoPassword.innerHTML="El campo PASSWORD se encuentra vacio"
+            parrafoPassword.innerHTML="El campo PASSWORD se encuentra vacio";
+            iconPassword.classList.remove('fa-cicle-check');
+            iconPassword.classList.add('fa-circle-xmark');
+            iconPassword.style.visibility="visible";
+            iconPassword.style.color="red";
+        
         }else{
 
              parrafoPassword.style.display="none";
             campoPassword.style.border="2.1px solid green";
+            iconPassword.classList.remove('fa-circle-xmark');
+            iconPassword.classList.add('fa-circle-check');
+          iconPassword.style.visibility="visible";
+          iconPassword.style.color="green";
 
          
 
