4d9dd42542164af0b6ba09886f9216d82c8789ce
mariadumas <dumasmaria@gmail.com>
2022-08-03 00:11:40 -0300

    vistas literatura novela infantiles

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -7,5 +7,8 @@ const mainController = require("../controllers/mainController")
 
 router.get("/", mainController.home);
 router.get('/viajes', mainController.viajes);
+router.get('/historieta', mainController.historieta);
+router.get('/literaturaIlustrada', mainController.literatura);
+router.get('/infantiles', mainController.infantiles);
 
 module.exports = router
