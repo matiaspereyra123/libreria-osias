c850c7b00d31e4d6596c8d9572d205f70a51b01d
mariadumas <dumasmaria@gmail.com>
2022-08-03 17:57:58 -0300

    config again

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -10,14 +10,14 @@ module.exports = {
     "username": "ioksom7lwnvm1d8s",
     "password": "xhdh7eee5a6s2xyl",
     "database": "ja180zuqe8r6xx3k",
-    "host": "cxmgkzhk95kfgbq4.0.0.1",
+    "host": "cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
   },
   "production": {
     "username": "ioksom7lwnvm1d8s",
     "password": "xhdh7eee5a6s2xyl",
     "database": "ja180zuqe8r6xx3k",
-    "host": "cxmgkzhk95kfgbq4",
+    "host": "cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     "dialect": "mysql"
   }
 }
