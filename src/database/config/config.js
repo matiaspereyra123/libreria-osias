cc98778815bbbe3fef98a74490990a91c14ef5bf
mariadumas <dumasmaria@gmail.com>
2022-08-03 17:44:03 -0300

    config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -7,10 +7,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "ioksom7lwnvm1d8s",
+    "password": "xhdh7eee5a6s2xyl",
+    "database": "ja180zuqe8r6xx3k",
+    "host": "cxmgkzhk95kfgbq4.0.0.1",
     "dialect": "mysql"
   },
   "production": {
