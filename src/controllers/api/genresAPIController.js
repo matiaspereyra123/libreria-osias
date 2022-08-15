e7f2f355d097f2a993681c10e9827d40799787e4
mariadumas <dumasmaria@gmail.com>
2022-08-15 19:58:26 -0300

    list2

--- src/controllers/api/genresAPIController.js
+++ src/controllers/api/genresAPIController.js
@@ -21,6 +21,27 @@ const genresAPIController={
                 res.json(respuesta);
             })
     },
+    'list2': (req, res) => {
+        db.Genero.findAll(({
+
+            include: [{
+                association: "libros", 
+                // attribute: "name"
+            }
+            ]}))
+        .then(genres => {
+            let respuesta = {
+                meta: {
+                    status : 200,
+                    total: genres.length,
+                    url: 'api/genres',
+                    table:"Géneros"
+                },
+                data: genres
+            }
+                res.json(genres[0].libros.length);
+            })
+    },
     
 
 }
